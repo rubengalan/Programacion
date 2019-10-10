bbe8ff9e5ca0ea66478172b7cf51582453956e0a
rubengalan <56016708+rubengalan@users.noreply.github.com>
2019-10-10 09:12:01 +0200

    Delete Ejercicio1.java

--- Tema 4/Tema 5/Ejercicio1.java
+++
@@ -1,9 +0,0 @@
-public class Ejercicio1 {
-    public static void main(String[] args) {
-      System.out.print("Los multiplos de 5 son: ");
-      for(int i = 0; i <= 100; i+=5) {
-          System.out.print(i + " ");
-      }
-    }
-  }
-  
