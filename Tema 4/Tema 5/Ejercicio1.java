a1e16bfb98ae257fca7b427989f98c8e78c12f47
rubengalan <56016708+rubengalan@users.noreply.github.com>
2019-10-10 09:09:29 +0200

    Add files via upload

---
+++ Tema 4/Tema 5/Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+      System.out.print("Los multiplos de 5 son: ");
+      for(int i = 0; i <= 100; i+=5) {
+          System.out.print(i + " ");
+      }
+    }
+  }
+  
