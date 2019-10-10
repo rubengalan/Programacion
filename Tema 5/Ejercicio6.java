ce77ca967bc2c4715e22615f1d69a56134521600
rubengalan <56016708+rubengalan@users.noreply.github.com>
2019-10-10 09:11:12 +0200

    Add files via upload

---
+++ Tema 5/Ejercicio6.java
@@ -0,0 +1,12 @@
+public class Ejercicio6 {
+  public static void main(String[] args) {
+    
+    int i = 320;
+    
+    do {
+      System.out.print(i + " ");
+      i-=20; 
+      }
+       while (i >= 160);
+  }
+}
